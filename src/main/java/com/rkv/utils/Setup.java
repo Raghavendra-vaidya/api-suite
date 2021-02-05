1440bd51b2f393a1a3768c36959629a3b3426235
Raghavendra vaidya <rkvaidya58@gmail.com>
2021-02-05 21:52:26 +0530

    chore(suite): updated pom.xml to remove scope of testng and jayway restasssured. updated Setup class :zap:

--- src/main/java/com/rkv/utils/Setup.java
+++ src/main/java/com/rkv/utils/Setup.java
@@ -1,4 +1,23 @@
 package com.rkv.utils;
 
+import com.jayway.restassured.RestAssured;
+import org.testng.annotations.BeforeSuite;
+
 public class Setup {
+
+    Configuration config = null;
+    public Configuration getConfigurationObject(){
+        return config;
+    }
+
+    @BeforeSuite
+
+    public void setup()
+    {
+        config = new Configuration();
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+
+
 }
