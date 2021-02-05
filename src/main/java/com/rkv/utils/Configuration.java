846dabb8870d00048cb0ef7842ec236a0760a469
Raghavendra vaidya <rkvaidya58@gmail.com>
2021-02-05 21:34:18 +0530

    Add configs and utils ⚡  (#2)
    * chore(suite): updated pom with dependancy and updated gitignore file :zap:
    
    * chore(suite): added config files and setup class :zap:

---
+++ src/main/java/com/rkv/utils/Configuration.java
@@ -0,0 +1,27 @@
+package com.rkv.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Configuration {
+    public  String url;
+
+    public Configuration(){
+        Properties prop = new Properties();
+        try {
+            FileInputStream file = new FileInputStream("./constants.properties");
+            prop.load(file);
+            this.url = prop.getProperty("URL");
+        }
+        catch (IOException i){
+            
+            i.printStackTrace();
+        }
+
+
+    }
+
+
+}
