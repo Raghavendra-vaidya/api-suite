75fd7b7a988606872f788e60bbcb30547daa34f5
Raghavendra vaidya <rkvaidya58@gmail.com>
2022-04-14 13:25:21 +0530

    chore(suite): add CI setup :rocket:

--- src/test/java/com/rkv/testscripts/PetsTest.java
+++ src/test/java/com/rkv/testscripts/PetsTest.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 
-public class PetsTests {
+public class PetsTest {
     Date date;
 
     @Test (description = "Create new pet and verify the creation")
--- src/test/java/com/rkv/testscripts/UserTest.java
+++ src/test/java/com/rkv/testscripts/UserTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
 
 import java.util.Date;
 
-public class UserTests {
+public class UserTest {
 
     Date date;
 
